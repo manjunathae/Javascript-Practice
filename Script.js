2e3d929a5e00932c694a57d727ba99435de09f43
Eshwarappa <Manjunath.Eshwarappa@xcmsolutions.com>
2019-12-20 13:12:33 +0530

    changes moved

--- Script.js
+++ Script.js
@@ -26,7 +26,7 @@ var Employee =[
     id:4,
     name:'jevetha',
     gender:'Female',
-    age:28,
+    age:21,
     salary:20000
 }
 
@@ -44,26 +44,48 @@ Array.prototype.FindInGivenArray =  function(name)
 
 // --method to fing max of salary in the given array---
 Array.prototype.findMaxage=function() {
-    var  max = Employee[0].age;
+//     var  max = Employee[0].age;
   
-    for (var i = 1;i<Employee.length;  i++) {
-      var v = Employee[i].age;
-      max = (v > max) ? v : max;
-    }
+//     for (var i = 1;i<Employee.length;  i++) {
+//       var v = Employee[i].age;
+//       max = (v > max) ? v : max;
+//     }
   
-    return max;
-  }
+//     return max;
+
+// New request Calulating max age without using loop
+
+
+Employee.sort(function(a,b)
+{
+    if(a.age > b.age) return 1;
+    if(a.age < b.age) return -1;
+    return 0;
+
+});
+return Employee[Employee.length-1].age;
+}
+
 
   //method to find minimum of salary in the given array--
   Array.prototype.findminage=function()
   {
-      var min=Employee[0].age;
-      for(var i=1;i<Employee.length;i++)
-      {
-          var v=Employee[i].age;
-          min = (v < min) ? v : min;
-      }
-      return min;
+    //   var min=Employee[0].age;
+    //   for(var i=1;i<Employee.length;i++)
+    //   {
+    //       var v=Employee[i].age;
+    //       min = (v < min) ? v : min;
+    //   }
+    //   return min;
+
+    Employee.sort(function(a,b)
+{
+    if(a.age > b.age) return 1;
+    if(a.age < b.age) return -1;
+    return 0;
+
+});
+   return Employee[0].age;
   }
 
   //method to insert new employee   to the the emolyee array
